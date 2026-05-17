466b2573f9405973dee8c254dcddc2d8ac48ff03
znt10 <jc970807@gmail.com>
2026-05-17 13:38:37 -0300

    fix gunicorn module

--- manage.py
+++ manage.py
@@ -4,8 +4,10 @@ import sys
 
 
 def main():
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-
+    os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE",
+    "backend.backend.settings"
+)
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
